f54724f17ea1b407da9561756a215e3927e2a04e
PraveenNarwal <praveennarwal5@gmail.com>
2022-05-01 21:42:14 +0530

    client added

---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBTypography } from "mdb-react-ui-kit";
+
+const Home = () => {
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "1000px",
+        alignContent: "center",
+      }}
+    >
+      <h5>Home page</h5>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/TagS.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import {
+  MDBCard,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardBody,
+  MDBCardImage,
+  MDBRow,
+  MDBCol,
+  MDBBtn,
+  MDBCardGroup,
+} from "mdb-react-ui-kit";
+const TagS = () => {
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "120px",
+        maxWidth: "900px",
+        alignContent: "center",
+      }}
+    ></div>
+  );
+};
+
+export default TagS;
